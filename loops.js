e162ce944020f8ed4031a568f86aff2c4737a208
Josephine Yao <yao.josephine@yahoo.com>
2019-01-09 21:34:09 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -38,6 +38,17 @@ function forLoop(array) {
 })*/
  
 function doWhileLoop(array) {
+function doWhileLoop(array) {
+   function maybeTrue() {
+     return Math.random() >= 0.5
+   }
+ 
+    do {
+     array.pop();
+    } 
+    while (array.length > -1 && maybeTrue());
+     return array;
+ }
  
     do {
      array.pop();
