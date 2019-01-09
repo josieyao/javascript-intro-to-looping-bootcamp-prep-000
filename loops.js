7a304ea8b4d3a2bc8b9adf0b6a32131adbbe6fdb
Josephine Yao <yao.josephine@yahoo.com>
2019-01-09 21:23:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,14 @@
+/*function makeArray() {
+  const array = []
+  const t = 10
+
+  for (let i = 0; i < t; i++) {
+    array.push("I am a strange loop.")
+  }
+
+  return [array, t]
+}*/
+
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
